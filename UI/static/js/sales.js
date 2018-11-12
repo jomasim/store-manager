053ba57ab6493b9de60bdeae0502c75bcd1f6743
Joma Simeon <simjoms@gmail.com>
2018-11-12 17:36:32 +0300

    [ft #161878200] fetch sales from api

--- UI/static/js/sales.js
+++ UI/static/js/sales.js
@@ -2,8 +2,9 @@ import ApiClient from './client.js'
 
 let client = new ApiClient()
 
-document.getElementById("checkout").addEventListener("click", createSale)
-
+if (document.getElementById("checkout")) {
+    document.getElementById("checkout").addEventListener("click", createSale)
+}
 
 function createSale(e) {
     e.preventDefault()
@@ -66,4 +67,80 @@ function createSale(e) {
 
 function getCheckout() {
     return localStorage.getItem("checkout")
+}
+
+if (document.getElementById("sales-table")) {
+    window.onload = getSales();
+}
+
+function getSales() {
+    client.get('sales')
+        .then(response => response.json().then(
+            payload => ({ status: response.status, body: payload })
+        ))
+        .then(payload => {
+            if (payload.status === 200) {
+                let sales = payload.body.sales
+                console.log(sales)
+                updateSalesTable(sales)
+            }
+        })
+}
+
+function salesHeader() {
+    let header = `
+    <tr>
+        <th>Sale ID</th>
+        <th>Product</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        <th>Total</th>
+        <th></th>
+        <th></th>
+    </tr>
+    `
+    return header
+}
+
+function updateSalesTable(sales) {
+    let table = document.getElementById("sales-table");
+    let header = salesHeader();
+    table.innerHTML = header
+    sales.forEach(sale => {
+        let date = getParsedDate(sale.create_at)
+            //divider
+        table.innerHTML += '<tr class="divider">' +
+            '<td colspan="5" >' + '</td>' +
+            '<td>' + "Attendant :" + sale.created_by + '</td>' +
+            '<td>' + date + '</td>' +
+            '</tr>'
+
+        sale.line_items.forEach(line_item => {
+            let selling_price = "Ksh " + line_item.selling_price + "/="
+            let total = Number(line_item.item_count) * Number(line_item.selling_price);
+            total = "Ksh " + total + "/="
+
+            table.innerHTML +=
+                `
+                <tr>
+                    <td>${sale.id}</td>
+                    <td>${line_item.product_id}</td>
+                    <td>${line_item.item_count}</td>
+                    <td>${selling_price}</td>
+                    <td>${total}</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            `
+        })
+    });
+
+}
+
+function getParsedDate(raw_date) {
+    let date = new Date(Date.parse(raw_date))
+    date = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + " \
+    " + date.getUTCHours() + ":" + date.getMinutes()
+
+    return date
 }
